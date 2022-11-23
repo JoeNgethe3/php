90cf946acbf9a271a1155c3027a266effcd69df9
JoeNgethe3 <JoeNgethe3@gmail.com>
2022-11-23 10:12:15 +0300

    This is a FizzBuzz program

---
+++ FizzBuzz.php
@@ -0,0 +1,16 @@
+<?php include "header.php" ?>
+<?php
+for($x=0;$x<=20;$x++){
+
+if($x%3==0 && $x%5==0){
+    echo "FizzBuzz <br>";
+}elseif($x%3==0){
+    echo "Fizz <br>";
+}elseif($x%5==0){
+    echo "Buzz <br>";
+}else{
+    echo "$x <br>";
+}
+}
+?>
+<?php include "footer.php" ?>
